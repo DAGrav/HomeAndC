8d8dc514c5dd2ff51e24b4b8a94e2f0886f490e5
Dmitriy Gerasimov <stock@grav-is.ru>
2025-02-08 09:33:44 +1000

    task3 commit

---
+++ HW1/task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("%4s\n","*"); //%Xs, где X - количество знакомест.
+    printf ("%5s\n","***");
+    printf ("%6s\n","*****");
+    printf ("%s\n","*******");
+    printf ("%6s\n","HHOHH");
+    printf ("%6s\n","ZZZZZ");
+    return 0;
+}
